a54698247b093b50f38d1b2c5468fb3714c86945
Anurag8960 <anurags05072000@gmail.com>
2025-02-27 01:06:53 +0530

    product service initialization

---
+++ src/test/java/com/yourdigitalnook/product_service/EcommerceProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourdigitalnook.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
